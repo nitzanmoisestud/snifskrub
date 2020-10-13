62611ce0b4deec01b9224c9ba035ed52b3fd00f7
suppmen <suppmen@gmail.com>
2020-10-13 14:55:00 +0200

    working on skrub functions

--- scrub.js
+++ scrub.js
@@ -1,60 +1,17 @@
-// document.addEventListener("DOMContentLoaded", start);
-
-// async function start() {
-//   let response = await fetch("/scrub-assets/background.svg");
-//   let mySvgData = await response.text();
-//   document.querySelector("#thesvg").innerHTML = mySvgData;
-//   startTheSvgClick();
-// }
-// function startTheSvgClick() {
-//   const shape = document.querySelector("#icons");
-//   shape.childNodes.forEach((each) => {
-//     each.addEventListener("click", clickedShape);
-//   });
-
-//   function clickedShape() {
-//     this.setAttribute("fill", "red");
-//   }
-// }
-
 "use strict";
 
 window.addEventListener("load", start);
+// Points arrays
+let elArray = [];
+let heatArray = [];
+let lightArray = [];
+let waterArray = [];
 
 // Global variables
 const rooms = document.querySelectorAll(".room-icon");
 
-function start() {
-  console.log("start");
-  document.querySelector("svg").setAttribute("viewBox", `0 0 1920 1080`);
-
-  rooms.forEach((room) => {
-    room.addEventListener("click", openRoom);
-  });
-}
-
-function openRoom(event) {
-  console.log(event.currentTarget.dataset.room);
-  let dataRoom = event.currentTarget.dataset.room;
-  const fox = document.querySelector(".snif");
-
-  fox.classList.add(`goto${dataRoom}`);
-
-  setTimeout(() => {
-    fox.classList.add("shrink");
-    console.log(fox);
-    renderBg(dataRoom);
-
-    loadElementsSvg(dataRoom);
-    rooms.forEach((room) => {
-      console.log(room);
-      room.style.display = "none";
-      fox.setAttribute("x", "50px");
-      fox.setAttribute("y", "50px");
-      fox.classList.remove(`goto${dataRoom}`);
-    });
-  }, 3000);
-}
+const background = document.getElementById("bgImage");
+const fox = document.querySelector(".snif");
 
 var bridge = document.querySelector("#bridge"),
   bridgeCanvas = bridge.getContext("2d"),
@@ -63,6 +20,134 @@ var bridge = document.querySelector("#bridge"),
 
 if (brushRadius > 100) {
   brushRadius = 100;
+}
+
+//start function to choose the rooms from index.html
+function start() {
+  console.log("start");
+  document.querySelector("svg").setAttribute("viewBox", `0 0 1920 1080`);
+
+  rooms.forEach((room) => {
+    room.addEventListener("click", openRoom);
+  });
+  const exitBtn = document.querySelector(".exit");
+  exitBtn.addEventListener("click", exitRoom);
+}
+
+function openRoom(event) {
+  let dataRoom = event.currentTarget.dataset.room;
+
+  setRooms(event);
+  fox.classList.add(`goto${dataRoom}`);
+  console.log(dataRoom);
+  setTimeout(() => {
+    fox.classList.add("shrink");
+    renderBg(dataRoom);
+
+    loadElementsSvg(dataRoom);
+    rooms.forEach((room) => {
+      room.style.display = "none";
+      fox.setAttribute("x", "50px");
+      fox.setAttribute("y", "50px");
+      fox.classList.remove(`goto${dataRoom}`);
+    });
+  }, 3000);
+}
+
+// Collecting points by clicking
+function startTheSvgClick() {
+  const shape = document.querySelector("#icons");
+  shape.childNodes.forEach((each) => {
+    each.addEventListener("click", clickedShape);
+  });
+  function clickedShape() {
+    this.classList.add("move");
+    const clicked = this.getAttribute("id");
+    console.log("clicked", clicked);
+    moveToArray(clicked);
+  }
+
+  function moveToArray(id) {
+    if (id === "el") {
+      elArray.push(this);
+      console.log("el", elArray.length);
+    } else if (id === "heat") {
+      heatArray.push(this);
+      console.log("heat", heatArray.length);
+    } else if (id === "light") {
+      lightArray.push(this);
+      console.log("light", lightArray.length);
+    } else if (id === "water") {
+      waterArray.push(this);
+      console.log("water", waterArray.length);
+    }
+  }
+}
+
+function renderToSvgBox() {
+  let use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+  use.setAttribute("href", "#domElements");
+  const elements = document.querySelector("#thesvg");
+  elements.appendChild(use);
+  console.log(use);
+}
+
+// Navigation
+
+function setRooms(event) {
+  // arrows to change room
+  let roomIndex = event.target.dataset.index;
+
+  const prevArrow = document.querySelector(".prev-arrow");
+  prevArrow.dataset.index = roomIndex;
+  prevArrow.addEventListener("click", prevRoom);
+  const nextArrow = document.querySelector(".next-arrow");
+
+  nextArrow.dataset.index = roomIndex;
+  console.log(nextArrow);
+  nextArrow.addEventListener("click", nextRoom);
+}
+
+function nextRoom(event) {
+  let index = event.target.dataset.index;
+  if (index < 3) {
+    event.target.dataset.index = parseInt(index) + 1;
+  } else {
+    event.target.dataset.index = 0;
+  }
+  console.log(index);
+  event.target.dataset.room = rooms[event.target.dataset.index].dataset.room;
+  changeRoom(event);
+}
+
+function prevRoom(event) {
+  let index = event.target.dataset.index;
+  if (index > 0) {
+    event.target.dataset.index = parseInt(index) - 1;
+  } else {
+    event.target.dataset.index = 3;
+  }
+  event.target.dataset.room = rooms[event.target.dataset.index].dataset.room;
+  changeRoom(event);
+}
+
+function changeRoom(event) {
+  let dataRoom = event.currentTarget.dataset.room;
+  const fox = document.querySelector(".snif");
+
+  renderBg(dataRoom);
+
+  loadElementsSvg(dataRoom);
+}
+
+function exitRoom(event) {
+  document.querySelector("svg").setAttribute("viewBox", `0 0 1920 1080`);
+
+  // img.style.display = "none";
+  rooms.forEach((room) => {
+    room.style.display = "block";
+    // ring.style.display = "none";
+  });
 }
 
 img.onload = function () {
